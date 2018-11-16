46bdf02faf63eb43cbc59f7595ae32e3bb05bf7f
Marek Dědič <developer@dedic.eu>
2018-11-16 16:57:07 +0100

    Nice output

--- src/main.py
+++ src/main.py
@@ -5,16 +5,28 @@ from data_stats import CDataStatsNoPreprocess, CDataStatsTrimmed, CDataStatsFram
 
 x, y = load('data/mnist.csv')
 
-noPreprocess = CDataStatsNoPreprocess()
-trimmed = CDataStatsTrimmed(30)
-frame = CDataStatsFrame(28, 28, 5)
+stats = [CDataStatsNoPreprocess(), CDataStatsTrimmed(30), CDataStatsFrame(28, 28, 5)]
+names = ['original', 'trimmed', 'framed']
+output = ''
 
 for i in range(3):
-    for stat in [frame]:
-        pyplot.imshow(x[i].reshape(28, 28))
-        pyplot.title('The ' + str(i + 1) + '. digit')
-        pyplot.show()
-        stat.histogram(x[i])
-        pyplot.title('Histogram for the ' + str(i + 1) + '. digit')
-        pyplot.show()
-        print(stat.mean(x[i]))
+    digit = x[i]
+    pyplot.figure(figsize = (12, 12))
+    pyplot.subplot(2, 2, 1)
+    pyplot.imshow(digit.reshape(28, 28))
+    pyplot.title('The ' + str(i + 1) + '. digit')
+    for j in range(len(stats)):
+        pyplot.subplot(2, 2, j + 2)
+        stat = stats[j]
+        stat.histogram(digit)
+        pyplot.title('Histogram, ' + names[j])
+        output += 'The mean of the ' + str(i + 1) + '. digit, ' + names[j] + ': ' + str(stat.mean(digit)) + '\n'
+    pyplot.savefig('digit' + str(i) + '.pdf', dpi = 1000)
+
+for j in range(len(stats)):
+    stat = stats[j]
+    output += 'The global mean, ' + names[j] + ': ' + str(stat.dataset_mean(x)) + '\n'
+
+file = open("output.txt", "w")
+file.write(output)
+file.close()
